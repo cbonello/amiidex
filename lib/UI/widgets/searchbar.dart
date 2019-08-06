2ddb80761c311fc48284b13cdefb5daac9a8dcd5
Christophe Bonello <christophe.bonello@gmail.com>
2019-08-06 15:24:28 -0400

    remove comments

--- lib/UI/widgets/searchbar.dart
+++ lib/UI/widgets/searchbar.dart
@@ -24,13 +24,7 @@ class SearchBar extends StatelessWidget {
       title: GestureDetector(
         child: Text(
           I18n.of(context).text('collection-search'),
-          // TODO(cbonello): use theme color.
           style: Theme.of(context).textTheme.subhead,
-
-          // TextStyle(
-          //   color: Colors.grey[500],
-          //   fontSize: 18,
-          // ),
         ),
         onTap: () async {
           await showSearch(
