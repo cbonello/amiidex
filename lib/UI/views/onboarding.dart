6630f0c1c441919bb7fd8774a4df9e71b3e2a6ff
Christophe Bonello <cbonello@users.noreply.github.com>
2020-09-19 15:56:30 -0400

    add translations of Done and Skip show that tutorial follows the what's new page

--- lib/UI/views/onboarding.dart
+++ lib/UI/views/onboarding.dart
@@ -5,10 +5,23 @@ import 'package:amiidex/util/i18n.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
-class OnboardingView extends StatelessWidget {
-  OnboardingView({Key key, this.config}) : super(key: key);
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({Key key, this.config}) : super(key: key);
 
   final ConfigModel config;
+
+  @override
+  _OnboardingViewState createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  int _currentPage;
+
+  @override
+  void initState() {
+    _currentPage = 0;
+    super.initState();
+  }
 
   final List<String> _assets = <String>[
     'assets/images/onboarding/whats_new.png',
@@ -43,7 +56,11 @@ class OnboardingView extends StatelessWidget {
   Future<void> _onSkipEnd(BuildContext context) async {
     final LocalStorageService storageService = locator<LocalStorageService>();
     await storageService.setDisplayOnboarding(false);
-    await Navigator.pushReplacementNamed(context, '/home', arguments: config);
+    await Navigator.pushReplacementNamed(
+      context,
+      '/home',
+      arguments: widget.config,
+    );
   }
 
   @override
@@ -52,12 +69,18 @@ class OnboardingView extends StatelessWidget {
       child: IntroductionScreen(
         onDone: () async => _onSkipEnd(context),
         onSkip: () => _onSkipEnd(context),
+        onChange: (int pageIndex) => setState(() => _currentPage = pageIndex),
         showSkipButton: true,
         skipFlex: 0,
         nextFlex: 0,
-        skip: const Text('Skip'),
-        next: const Icon(Icons.arrow_forward),
-        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+        skip: Text(I18n.of(context).text('onboarding-skip')),
+        next: _currentPage == 0
+            ? Text(I18n.of(context).text('onboarding-tutorial'))
+            : const Icon(Icons.arrow_forward),
+        done: Text(
+          I18n.of(context).text('onboarding-done'),
+          style: const TextStyle(fontWeight: FontWeight.w600),
+        ),
         pages: <PageViewModel>[
           for (int index = 0; index < _assets.length; index++)
             PageViewModel(
